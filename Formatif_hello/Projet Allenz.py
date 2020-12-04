c9bdb609cbf63d3bded408da8a49d67ab4be0f23
unknown <cecce@FRCLOC203-67403.ecolecatholique.ca>
2020-12-04 10:00:07 -0500

    reorganized

---
+++ Formatif_hello/Projet Allenz.py
@@ -0,0 +1,7 @@
+name = input("What is your name?: ")
+print("hello:" + name)
+print("I will now see if you are allowed into the net.")
+birth_year = input("Enter your birth year: ")
+age = 2020 - int(birth_year)
+print(age) 
+print("approved. welcome: " + name )
