0e00d77b8b9d88517788b71924405edbcf75d326
allenzpierre <pieall25@ecolecatholique.ca>
2020-12-04 14:55:19 +0000

    Added comments.

--- allenz_thing/projet.py
+++ allenz_thing/projet.py
@@ -1,11 +1,20 @@
+#ce program represent une simulation d'un log in 
+#Allenz Alberson Pierre
+#4 dec. 2020
+
+#Demander Le nom
 name = input("What is your name?: ")
 print("hello:" + name)
+
+#Validation de l'age
 print("I will now see if you are allowed into the net.")
 birth_year = input("Enter your birth year: ")
 age = 2020 - int(birth_year)
 print(age)
+#Acceptation si l'age est plus que 17.
 if age >= 17
 else = print("access denied.")
 print("approved. welcome: " + name )
+#Debut de L'application.
 input("what can we do for you today?: ")
 print("Loading.....")
